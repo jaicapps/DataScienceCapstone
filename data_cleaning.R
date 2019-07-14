050783cb28754a32734cd16deec0a093363cbf8c
grzechowiak <grzechowiak@users.noreply.github.com>
2019-07-14 16:42:31 -0500

    start off

--- data_cleaning.R
+++
@@ -1,7 +0,0 @@
-url <- "https://www1.nyc.gov/assets/planning/download/zip/data-maps/open-data/nyc_pluto_18v2_1_csv.zip"
-filename <- "pluto_data.zip"
-download.file(url, filename)
-files <- unzip(filename)
-df <- read.csv("pluto_18v2_1.csv")
-file.remove(c(filename, "PLUTODD18v2.1.pdf", "PlutoReadme18v2.1.pdf"))
-colnames(df)
