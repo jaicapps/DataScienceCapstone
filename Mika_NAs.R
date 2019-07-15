baeb3e4f35c49eeee49212ba1f2075dcc16e9fe6
mikaelapisani <mikaelapisani@outlook.com>
2019-07-14 19:02:11 -0500

    fixed levels

--- Mika_NAs.R
+++ Mika_NAs.R
@@ -3,7 +3,8 @@ colnames(df)
 #borough
 summary(df$borough)
 table(is.na(df$borough))
-#delete levels that are large 
+#delete levels that are large
+df$borough <- as.factor(df$borough)
 l <- levels(df$borough)
 #check amount of rows that have rare borough 
 dim(df[df$borough==l[1],])
@@ -21,6 +22,7 @@ df <- df[!(df$borough==l[4]), ]
 df <- df[!(df$borough==l[5]), ]
 df <- df[!(df$borough==l[6]), ]
 dim(df)
+df <- droplevels(df)
 
 ########
 #block
@@ -32,8 +34,11 @@ table(is.na(df$lot))
 #cd
 table(is.na(df$cd))
 cd_per_borough <- unique(df[c("borough", "cd")])
-cd_per_borough <- cd_per_borough[!(is.na(cd_per_borough))]
-sample(cd_per_borough, 1)
+cd_per_borough <- cd_per_borough[!(is.na(cd_per_borough$cd)), ]
+cd_BX <- cd_per_borough[cd_per_borough$borough=="BX",]$cd
+cd_BK <- cd_per_borough[cd_per_borough$borough=="BK",]$cd
+sample(cd_BX, 1)
+df2 <- df
 
-
+df2[is.na(df2) & df2$borough=="BX"] <- sample(cd_BX, 1)
 
