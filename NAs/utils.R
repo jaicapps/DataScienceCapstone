92e85e5ac54dc89aa9817503684c53017b77ede2
mikaelapisani <mikaelapisani@outlook.com>
2019-07-18 16:31:49 -0500

    fixed fill NAs functions

--- NAs/utils.R
+++ NAs/utils.R
@@ -1,20 +1,27 @@
 get_sample<-function(b, col_per_zipcode, colname) { #get a sample where the zipcode is equal to the received 
-  return (sample(col_per_zipcode[colname][col_per_zipcode["zipcode"]==b],1)) 
+  list_to_sample <- col_per_zipcode[colname][col_per_zipcode["zipcode"]==b]
+  if (length(list_to_sample)==0) {
+    return (NA)
+  }
+  return (sample(list_to_sample, size=1)) 
 }
 
 fill_NAs_by_zipcode <- function(df, colname) {
   col_per_zipcode <- unique(df[c("zipcode", colname)]) #keep unique values for each zipcode
   col_per_zipcode <- col_per_zipcode[!(is.na(col_per_zipcode[colname])), ] #remove NAs
-  df[colname][is.na(df[colname])] <- unlist(lapply(df["zipcode"][is.na(df[colname])], FUN=get_sample, col_per_zipcode, colname))
+  return (sapply(df["zipcode"][is.na(df[colname])], FUN=get_sample, col_per_zipcode, colname))
 }
 
-#install.packages("dplyr")
 library(dplyr)
 get_median<-function(b, col_per_zipcode, colname){
-  return (col_per_zipcode[which(col_per_zipcode["zipcode"]==b), colname])
+  index <- which(col_per_zipcode["zipcode"]==b)
+  if (length(index)==0) {
+    return (NA)
+  }
+  return (col_per_zipcode[index, colname])
 }
 fill_NAs_median<-function(df, colname) {
   col_per_zipcode <- df[c("zipcode", colname)] %>% group_by_at("zipcode") %>% na.omit()  %>% summarise_at(vars(colname), median)
-  df[colname][is.na(df[colname])] <- unlist(lapply(df["zipcode"][is.na(df[colname])], FUN=get_median, col_per_zipcode, colname))
+  return (sapply(df["zipcode"][is.na(df[colname])], FUN=get_median, col_per_zipcode, colname))
 }
 
--- correlation_Mika.R
+++ correlation_Mika.R
@@ -1,10 +1,24 @@
 df <- read.csv("pluto2.csv")
+
+#chisq.test example
 df$borocode <- as.factor(df$borocode)
 df$healtharea <- as.factor(df$healtharea)
 colnames(df)
 chisq.test(df$healthcenterdistrict, df$borocode, simulate.p.value = TRUE)
 
-
+#keep variables related with location 
 location_vars <- c("schooldist","council","zipcode","firecomp",
-          "policeprct","healtharea","sanitboro","sanitsub","zonedist1","spdist1",
-           "irrlotcode","borocode","sanitdistrict","healthcenterdistrict","pfirm15_flag")
+                   "policeprct","healtharea","sanitboro","sanitsub","zonedist1","spdist1",
+                   "irrlotcode","borocode","sanitdistrict","healthcenterdistrict","pfirm15_flag")
+
+#test correlation with assestot
+library(dplyr)
+df2 <- select(df, c("assesstot","assessland", location_vars))
+df2[location_vars] <- lapply(df2[location_vars], factor)  
+
+fit <- aov(assesstot ~ schooldist, data=df2)
+
+
+
+
+
