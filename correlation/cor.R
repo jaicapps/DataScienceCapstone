575cad3597ce1c6b7afcadcebb719cde26bd1f95
mikaelapisani <mikaelapisani@outlook.com>
2019-07-21 16:00:37 -0500

    add outliers code

--- correlation/cor.R
+++ correlation/cor.R
@@ -1,3 +1,4 @@
+source("libraries.R")
 df <- read.csv("pluto3.csv")
 col_var <- c("block","lot","cd","schooldist","council","zipcode","firecomp","policeprct",
              "healtharea","sanitboro","sanitsub","zonedist1","spdist1","ltdheight","landuse",
@@ -5,3 +6,5 @@ col_var <- c("block","lot","cd","schooldist","council","zipcode","firecomp","pol
              "healthcenterdistrict", "pfirm15_flag")
 #keep numeric vars
 df <- select(df, -c(col_var))
+
+cor(df)
