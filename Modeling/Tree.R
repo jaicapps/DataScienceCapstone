a77ec78fa1f1e3fc2ff6226def032478cbc8fc82
grzechowiak <grzechowiak@users.noreply.github.com>
2019-07-20 20:07:22 -0500

    Importance of varbiables-tree

--- Modeling/Tree.R
+++ Modeling/Tree.R
@@ -1,7 +1,17 @@
-library(dplyr)
-df2 <- na.omit(df)
-sample <- sample_n(df2, 100)
+# Read data and change types:
+df <- read.csv("pluto2.csv")
+source("data_type_fun.R")
+source("libraries.R")
+df <- data_type(df)
+
+
+# Choose columns with small level (lower than 53)
+df <- subset(df ,select = -c(block, lot, zipcode, firecomp, policeprct, healtharea,sanitsub))
+
+# Sample for test
+sample <- sample_n(df, 100)
 
 library(randomForest)
 rf <- randomForest(assesstot ~ ., data = sample, ntree = 500, nodesize = 5, importance = TRUE)  
 # Can not handle categorical predictors with more than 53 categories.
+varImpPlot(rf, type = 1)
