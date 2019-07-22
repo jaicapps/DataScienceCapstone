8d33776dcebf2030ef87900d19330d0dbcc1c465
Arjuna-Menon <30682967+Arjuna-Menon@users.noreply.github.com>
2019-07-22 12:41:09 -0500

    Create pca.R

---
+++ pca.R
@@ -0,0 +1,12 @@
+source("libraries.R")
+df <- read.csv("pluto3.csv")
+col_var <- c("block","lot","cd","schooldist","council","zipcode","firecomp","policeprct",
+             "healtharea","sanitboro","sanitsub","zonedist1","spdist1","ltdheight","landuse",
+             "ext","proxcode","irrlotcode","lottype","borocode","edesignum","sanitdistrict",
+             "healthcenterdistrict", "pfirm15_flag")
+#keep numeric vars
+df <- select(df, -c(col_var, "xcoord", "ycoord"))
+df <- scale(df)
+summary(df)
+df.pca <- princomp(df, cor = T)
+df.pca$loadings
