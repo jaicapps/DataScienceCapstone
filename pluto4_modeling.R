f8e5daf93f8c357cd8f6722ecada35c906eaa418
Arjuna-Menon <30682967+Arjuna-Menon@users.noreply.github.com>
2019-07-25 13:59:06 -0500

    Updates

--- pluto4_modeling.R
+++ pluto4_modeling.R
@@ -1,12 +1,3 @@
-library("dplyr")
-df <- read.csv("pluto2.csv")
-table(df$zipcode==11430) # Outliers as seen from Tableau. JFK airport that has too high of an 
-# assessed land and assessed total
-table(df$zipcode == 12345) # Outlier as seen from Tableau.
-df <- filter(df, !(zipcode == 11430 | zipcode == 12345))
-# writing to csv:
-write.csv(df, file = "pluto2.csv", row.names=FALSE)
-
 # From the correlation of numerical variables, we decide not to delete any numerical variables.
 
 # From the chi-squared test for independence between categorical variables, we delete the following:
