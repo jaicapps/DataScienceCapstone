66d17888029c142fe95fc0e85da2115096a4e98f
Mikaela Pisani <mpisani@netlabs.com.uy>
2019-07-25 14:06:09 -0500

    create samplelefor all vars

---
+++ sample/get_sample.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 25 13:51:39 2019
+
+@author: mikaelapisanileal
+"""
+
+from proportion_sample import create_sample
+import pandas as pd
+
+df = pd.read_csv("../pluto2.csv")
+df_assestot = pd.DataFrame.copy(df)
+X_train, X_test, y_train, y_test, predictors = create_sample(df, "assessland", 0.1, 0.025)
+
+df1 = pd.DataFrame(X_train)
+df1.columns = predictors
+df1["assessland"] = pd.DataFrame(y_train)
+
+df2 = pd.DataFrame(X_test)
+df2.columns = predictors
+df2["assessland"] = pd.DataFrame(y_test)
+
+df = pd.concat([df1, df2])
+df.to_csv("sample.csv", index=False)
