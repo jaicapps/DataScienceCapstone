315b1b612d1c3a5f17230afae43d69fb7a597e61
Mikaela Pisani <mpisani@netlabs.com.uy>
2019-07-24 23:52:22 -0500

    add model comparation

--- feature_selection/Mikaela_example_glmnet.R
+++ feature_selection/Mikaela_example_glmnet.R
@@ -12,50 +12,120 @@ no_cores <- max(1, detectCores() - 2)
 df <- read.csv("pluto4.csv")
 df <- data_type(df)
 
+execute_lasso_assesstot <- function(df) {
+  # Split the data into training and test set
+  set.seed(123)
+  training.samples <- df$assesstot %>% createDataPartition(p = 0.8, list = FALSE)
+  train.data  <- df[training.samples, ]
+  test.data <- df[-training.samples, ]
+  
+  # Predictor variables: transform categorical to dummpy 
+  x <- model.matrix(assesstot~., train.data)[,-1]
+  
+  # Outcome variable
+  y <- train.data$assesstot
+  
+  # Find the best lambda using cross-validation (lambda is to penalize)
+  set.seed(1234)
+  registerDoMC(cores=no_cores)
+  cv <- cv.glmnet(x, y, alpha = 1, parallel = TRUE) # lasso regression alpha=1
+  # Display the best lambda value
+  print(paste("min lambda", cv$lambda.min))
+  
+  print("Fitting final model on the training data")
+  # Fit the final model on the training data
+  model <- glmnet(x, y, alpha = 1, lambda = cv$lambda.min)
+  print("Predictions on the test data")
+  x.test <- model.matrix(assesstot ~., test.data)[,-1]
+  predictions_coef <- model %>% predict(newdata=x.test, type="coefficient") 
+  print(predictions_coef) #the coef=0 means that the variables are not significant
+  predictions <- model %>% predict(x.test) %>% as.vector()
+  error = mean((predictions - test.data$assesstot)^2)
+  
+  # Model performance metrics
+  RMSE = RMSE(predictions, test.data$assesstot)
+  R2 = R2(predictions, test.data$assesstot)
+  
+  print(paste("error=", error))
+  print(paste("RMSE=", RMSE))
+  print(paste("R2=", R2))
+  
+  return (list(error,RMSE, R2))
+}
+
+
+execute_lasso_assessland <- function(df) {
+  # Split the data into training and test set
+  set.seed(123)
+  training.samples <- df$assessland %>% createDataPartition(p = 0.8, list = FALSE)
+  train.data  <- df[training.samples, ]
+  test.data <- df[-training.samples, ]
+  
+  # Predictor variables: transform categorical to dummpy 
+  x <- model.matrix(assessland~., train.data)[,-1]
+  
+  # Outcome variable
+  y <- train.data$assessland
+  
+  # Find the best lambda using cross-validation (lambda is to penalize)
+  set.seed(1234)
+  registerDoMC(cores=no_cores)
+  cv <- cv.glmnet(x, y, alpha = 1, parallel = TRUE) # lasso regression alpha=1
+  # Display the best lambda value
+  print(paste("min lambda", cv$lambda.min))
+  
+  print("Fitting final model on the training data")
+  # Fit the final model on the training data
+  model <- glmnet(x, y, alpha = 1, lambda = cv$lambda.min)
+  print("Predictions on the test data")
+  x.test <- model.matrix(assessland ~., test.data)[,-1]
+  predictions_coef <- model %>% predict(newdata=x.test, type="coefficient") 
+  print(predictions_coef) #the coef=0 means that the variables are not significant
+  predictions <- model %>% predict(x.test) %>% as.vector()
+  error = mean((predictions - test.data$assessland)^2)
+  
+  # Model performance metrics
+  RMSE = RMSE(predictions, test.data$assessland)
+  R2 = R2(predictions, test.data$assessland)
+  
+  print(paste("error=", error))
+  print(paste("RMSE=", RMSE))
+  print(paste("R2=", R2))
+
+  return (list(error,RMSE, R2))
+}
+
+
 dep_var1 = c("cd", "council", "firecomp", "policeprct", "healtharea",
            "sanitboro", "sanitsub", "sanitdistrict", "healthcenterdistrict", 
            "block", "xcoord", "ycoord","block", "lot", "borocode",
-           "xcoord", "ycoord", "assessland", "zipcode")
-
+           "xcoord", "ycoord", "zipcode")
 dep_var2 = c("facilfar", "schooldist", "zonedist1", "spdist1", 
              "landuse", "ext", "proxcode", "irrlotcode", "edesignum", "pfirm15_flag")
+dep_var3 <- c("irrlotcode", "numfloors", "unitsres", "lotfront", "bldgfront", "bldgdepth",
+              "yearbuilt", "residfar", "commfar", "facilfar", "yearalter", "income")
 
-df <- df[, !(colnames(df) %in% dep_var1)]
-df <- df[,!(colnames(df) %in% dep_var2)]
-# Split the data into training and test set
-set.seed(123)
-training.samples <- df$assesstot %>% createDataPartition(p = 0.8, list = FALSE)
-train.data  <- df[training.samples, ]
-test.data <- df[-training.samples, ]
+df1 <- df[, !(colnames(df) %in% c(dep_var1, "assessland"))]
+errors_1 = execute_lasso_assesstot(df1)
 
-# Predictor variables: transform categorical to dummpy 
-x <- model.matrix(assesstot~., train.data)[,-1]
+df2 <- df1[,!(colnames(df1) %in% dep_var2)]
+errors_2 = execute_lasso_assesstot(df2)
 
-# Outcome variable
-y <- train.data$assesstot
+df3 <- df1[,!(colnames(df1) %in% dep_var3)]
+errors_3 = execute_lasso_assesstot(df3)
 
-# Find the best lambda using cross-validation (lambda is to penalize)
-set.seed(123)
-registerDoMC(cores=no_cores)
-cv <- cv.glmnet(x, y, alpha = 1, parallel = TRUE) # lasso regression alpha=1
-# Display the best lambda value
-print(paste("min lambda", cv$lambda.min))
+df4 <- df[, !(colnames(df) %in% c(dep_var1, "assesstot"))]
+errors_4 = execute_lasso_assessland(df4)
 
-print("Fitting final model on the training data")
-# Fit the final model on the training data
-model <- glmnet(x, y, alpha = 1, lambda = cv$lambda.min)
-# Display regression coefficients
-print("Coefficients:")
-c <- as.data.frame(coef(model))
-print(coef(model))
+df5 <- df4[, !(colnames(df4) %in% c(dep_var1, "assesstot"))]
+errors_5 = execute_lasso_assessland(df5)
 
-print("Predictions on the test data")
-x.test <- model.matrix(assesstot ~., test.data)[,-1]
-predictions <- model %>% predict(x.test) %>% as.vector()
-# Model performance metrics
-result <- data.frame(
-  RMSE = RMSE(predictions, test.data$assesstot),
-  Rsquare = R2(predictions, test.data$assesstot)
-)
-write.csv(result, "result.csv")
+df6 <- df4[, !(colnames(df4) %in% c(dep_var1, "assesstot"))]
+errors_6 = execute_lasso_assessland(df6)
+
+plot(c(unlist(errors_1[2]), unlist(errors_2[2]), 
+       unlist(errors_1_3[2]), unlist(errors_4[2]), unlist(errors_5[2]), 
+       unlist(errors_6[2])), main = "Compare RMSE", 
+     xlab = "Model", ylab="RMSE")
+
 
