8662dc199febb60485db060d9fc8ecc9428c500a
Arjuna-Menon <30682967+Arjuna-Menon@users.noreply.github.com>
2019-07-28 13:14:45 -0500

    New

--- sample/get_sample.py
+++ sample/get_sample.py
@@ -9,15 +9,12 @@ Created on Thu Jul 25 13:51:39 2019
 from proportion_sample import create_sample
 import pandas as pd
 
-df = pd.read_csv("../pluto2.csv")
-X_train, X_test, y_train, y_test, predictors = create_sample(df, "assessland", 0.011, 0.00075)
+df = pd.read_csv("../pluto4.csv")
+X_train, X_test, y_train, y_test, predictors = create_sample(df, "assessland", 0.02, 0.005)
 
 df = pd.DataFrame(X_train)
 df.columns = predictors
 df["assessland"] = pd.DataFrame(y_train)
-
-#Drop X and Y
-df.drop(['xcoord', 'ycoord'], axis=1, inplace=True)
 
 #Save data
 df.to_csv("sample_0.011.csv", index=False)
