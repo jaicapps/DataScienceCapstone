9bd2613892a590071a985bd6fafa55b4d84ce8b3
Arjuna-Menon <30682967+Arjuna-Menon@users.noreply.github.com>
2019-07-28 17:45:25 -0500

    pca updated

--- pca.R
+++ pca.R
@@ -1,17 +1,11 @@
 source("libraries.R")
-df <- read.csv("pluto3.csv")
+df <- read.csv("pluto4.csv")
 col_var <- c("block","lot","cd","schooldist","council","zipcode","firecomp","policeprct",
              "healtharea","sanitboro","sanitsub","zonedist1","spdist1","ltdheight","landuse",
              "ext","proxcode","irrlotcode","lottype","borocode","edesignum","sanitdistrict",
              "healthcenterdistrict", "pfirm15_flag")
 
-# Keep numeric variables for assessland prediction:
-df <- select(df, -c(col_var, "xcoord", "ycoord", "assessland"))
-df.pca <- princomp(df, cor = T) # cor=T for scaled data
-df.pca$loadings
-
-# Keep numeric variables for assesstot prediction:
-df <- read.csv("pluto3.csv")
-df <- select(df, -c(col_var, "xcoord", "ycoord", "assesstot"))
+# Keep numeric variables for assessland and assesstot predictions:
+df <- select(df, -c(col_var, "assessland", "assesstot"))
 df.pca <- princomp(df, cor = T) # cor=T for scaled data
 df.pca$loadings
